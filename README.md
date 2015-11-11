53e56aaf7ef322395c1200d11bdcbf8c47c75f66
Takao NAITO <xarsh@outlook.com>
2015-11-11 15:54:26 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+Docker Machine on Azure
+=====
+
+Credential files are encrypted with OpenSSL (aes-256-cbc).
+
+
+Run `./create.sh`.
---
+++ create.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Register mycert.cer file to Azure first."
+
+azure account list
+
+echo "Subscriotion ID: \c"
+read subscription
+echo "Password: \c"
+read -s password
+
+openssl aes-256-cbc -d -in mycert.cer.encrypted -out mycert.cer -pass pass:$password
+openssl aes-256-cbc -d -in mycert.pem.encrypted -out mycert.pem -pass pass:$password
+openssl aes-256-cbc -d -in mycert.pfx.encrypted -out mycert.pfx -pass pass:$password
+
+docker-machine create -d azure \
+--azure-subscription-id="$subscription" \
+--azure-subscription-cert="mycert.pem" \
+--azure-location="Japan West" \
+xarsh-docker-machine-base
+
+docker-machine env xarsh-docker-machine-base
